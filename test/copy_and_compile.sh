f3ae76e59c1032026e86c576662add13efef45cc
Jorge Pérez Zerpa <jorgepz@fing.edu.uy>
2023-09-19 22:23:39 -0300

    propose test document and CI

---
+++ test/copy_and_compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cp ../support/*.cls .
+cp ../support/document/*.tex .
+cp ../support/document/tex/*.tex .
+cp ../support/document/fig/* .
+cp ../support/export/* . 
+cp ../support/images/* .
+cp ../support/packages/*.tex .
+cp ../support/packages/mcode/*.sty .
+cp ../support/templates/*.tex . 
+cp ../support/templates/aiaa/* .
+cp ../support/templates/standalone/* .
+cp ../support/templates/tech/* .
+cp ../support/citations/aiaa/* .
